8f1d5328172bf7b02c75a3af0cbf3e7a7b5c259b
akopadung <akopadung>
2020-03-30 19:08:40 +0700

    Updates

---
+++ precache-manifest.c69cfb20974792a06e6cbd1d7ed6c634.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a280e4d57dbb7e835ee9100e67274e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "d12e294cf059d77073bf",
+    "url": "/test/static/css/main.af24128d.chunk.css"
+  },
+  {
+    "revision": "e8c2e43ed7eaa221c8ad",
+    "url": "/test/static/js/2.e1e539c4.chunk.js"
+  },
+  {
+    "revision": "e0cc70e3e6650053c84cf3323a3b8dd9",
+    "url": "/test/static/js/2.e1e539c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12e294cf059d77073bf",
+    "url": "/test/static/js/main.c035dcfc.chunk.js"
+  },
+  {
+    "revision": "d8b73bff85fd3246922d",
+    "url": "/test/static/js/runtime-main.b043ed5c.js"
+  },
+  {
+    "revision": "8eaa53ab12737df476ec37173e40ac6e",
+    "url": "/test/static/media/about.8eaa53ab.jpg"
+  },
+  {
+    "revision": "fb95d7130f66e3792136167b39aed951",
+    "url": "/test/static/media/img_bg_slide0.fb95d713.jpg"
+  },
+  {
+    "revision": "2e01b436a72d0a351efcaf820b98cfa6",
+    "url": "/test/static/media/img_bg_slide_1.2e01b436.JPG"
+  },
+  {
+    "revision": "5bbce33e2725f3d5888fc2ac52ee54f1",
+    "url": "/test/static/media/img_bg_slide_2.5bbce33e.jpg"
+  }
+]);
